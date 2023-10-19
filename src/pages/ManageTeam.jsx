370fd6faf98e37b3d6a41512cb41bbd381c28ea5
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-19 21:09:33 +0100

    manageTeam compo changes

--- src/pages/ManageTeam.jsx
+++ src/pages/ManageTeam.jsx
@@ -39,33 +39,45 @@ const rows = [
   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 10 },
+  { id: 6, lastName: "Melisandre", firstName: "Hamza", age: 150 },
   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 10, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 11, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 12, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 13, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 10, lastName: "Hnad", firstName: "Mohameed", age: 65 },
+  { id: 11, lastName: "smtha", firstName: "Harvad", age: 65 },
+  { id: 12, lastName: "Hamani", firstName: "Yassin", age: 65 },
+  { id: 13, lastName: "Yadiw", firstName: "James", age: 65 },
+  { id: 14, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 15, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 16, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 17, lastName: "Stark", firstName: "Arya", age: 16 },
 ];
 
 function ManageTeam() {
   return (
-    <Box sx={{ height: 400, width: "100%" }}>
+    <Box
+      sx={{
+        height: 630,
+        width: "100%",
+        // height: "1000px",
+        backgroundColor: "#457ce2",
+      }}
+    >
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
         pageSizeOptions={[5]}
         checkboxSelection
         disableRowSelectionOnClick
+        sx={{ color: "white" }}
       />
     </Box>
   );
